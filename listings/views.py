a6121f6786cf7e5713fe8eeb6001bd22d1c058f3
Dhaura Pathirana <dhaurapathirana@gmail.com>
2021-05-08 23:55:01 +0530

    pagination done for listings | dynamic realtors added

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,18 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 
 from .models import Listing
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+
+    page = request.GET.get('page')
+    page_listings = paginator.get_page(page)
 
     context = {
-        'listings': listings
+        'listings': page_listings
     }
 
     return render(request, 'listings/listings.html', context)
