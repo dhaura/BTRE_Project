98afae3eb9ccbf2cc37a2d36a4618998a841d601
Dhaura Pathirana <dhaurapathirana@gmail.com>
2021-05-18 22:29:10 +0530

    user registrations added

--- BTRE_Project/settings.py
+++ BTRE_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -134,3 +136,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
---
+++ accounts/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+
+def register(request):
+    if request.method == 'POST':
+
+        # get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # check if passwords match
+        if password == password2:
+            # check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                # check email
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request, 'You are now registered')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        print('login')
+        return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
