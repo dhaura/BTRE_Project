7c8aae38f46e23d22a5d5caefa6439e01918e09b
Dhaura Pathirana <dhaurapathirana@gmail.com>
2021-05-29 20:15:43 +0530

    email disabled

--- BTRE_Project/settings.py
+++ BTRE_Project/settings.py
@@ -149,11 +149,11 @@ MESSAGE_TAGS = {
 
 # Email config
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''  # should add the email
-EMAIL_HOST_PASSWORD = ''  # should add the password
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''  # should add the email
+# EMAIL_HOST_PASSWORD = ''  # should add the password
+# EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
